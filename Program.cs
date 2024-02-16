715e6648bbaa7e2335173ac95727590eaa3b41cb
Belginbalakrishanan <belginbalakrishnan@gmail.com>
2024-02-16 09:32:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("welcome to number system");
+Console.WriteLine("******EVEN NUMBER******");
+NumberSystem.EvenNum();
+Console.WriteLine("******ODD NUMBER******");
+NumberSystem.OddNum();  
